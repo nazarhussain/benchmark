3c9504039e71bc7edf670900db08214d6216f3ac
dapplion <35266934+dapplion@users.noreply.github.com>
2021-06-25 21:15:05 +0200

    Allow to run regular it() with benchmark

--- src/mochaPlugin/globalState.ts
+++ src/mochaPlugin/globalState.ts
@@ -4,10 +4,4 @@ import {BenchmarkResult} from "../types";
  * Map of results by root suie.
  * Before running mocha, you must register the root suite here
  */
-export const resultsByRootSuite = new WeakMap<Mocha.Suite, BenchmarkResult[]>();
-
-/**
- * Map of test instance to results.
- * Required to provide results to the custom reporter
- */
-export const testResults = new WeakMap<Mocha.Runnable, BenchmarkResult>();
+export const resultsByRootSuite = new WeakMap<Mocha.Suite, Map<string, BenchmarkResult>>();
